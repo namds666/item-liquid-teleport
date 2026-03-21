5a94209d2ec5e642ea1471ff393783fbaa439089
Dương Sơn Nam <namds666@gmail.com>
2026-03-22 00:13:02 +0700

    fix tree

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,10 @@ const chronoPusher         = require("chrono-pusher");
 const chronoLiquidUnloader = require("chrono-liquid-unloader");
 const chronoLiquidPusher   = require("chrono-liquid-pusher");
 
-const cost = ItemStack.with(Items.copper, 1, Items.lead, 1);
-
-new TechTree.TechNode(TechTree.get(Blocks.conveyor), chronoUnloader,       cost);
-new TechTree.TechNode(TechTree.get(Blocks.conveyor), chronoPusher,         cost);
-new TechTree.TechNode(TechTree.get(Blocks.conduit),  chronoLiquidUnloader, cost);
-new TechTree.TechNode(TechTree.get(Blocks.conduit),  chronoLiquidPusher,   cost);
+Events.on(ClientLoadEvent, cons(e => {
+    const cost = ItemStack.with(Items.copper, 1, Items.lead, 1);
+    new TechTree.TechNode(TechTree.get(Blocks.conveyor), chronoUnloader,       cost);
+    new TechTree.TechNode(TechTree.get(Blocks.conveyor), chronoPusher,         cost);
+    new TechTree.TechNode(TechTree.get(Blocks.conduit),  chronoLiquidUnloader, cost);
+    new TechTree.TechNode(TechTree.get(Blocks.conduit),  chronoLiquidPusher,   cost);
+}));
