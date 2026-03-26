d798d76d88884335f6e74402f2d75f850eb0f85b
Dương Sơn Nam <namds666@gmail.com>
2026-03-27 00:27:52 +0700

    add filters

--- scripts/lib.js
+++ scripts/lib.js
@@ -1,19 +1,22 @@
 exports.modName = "item-liquid-teleport";
 
 const CHRONO_NAMES = ["chrono-pusher", "chrono-unloader", "chrono-liquid-pusher", "chrono-liquid-unloader"].map(n => exports.modName + "-" + n);
-exports.autoConnectAll = (the, getLinks, lvt) => {
+const autoConnect = (the, getLinks, lvt, filter) => {
     let links = getLinks();
     Groups.build.each(cons(b => {
         if (b == the) return;
         if (CHRONO_NAMES.indexOf(b.block.name) >= 0) return;
+        if (filter && !filter(b)) return;
         if (!lvt(the, b)) return;
         let int = new java.lang.Integer(b.pos());
         if (!links.contains(boolf(i => i == int))) the.configure(int);
     }));
 };
 exports.addAutoConnectButtons = (table, the, getLinks, lvt, clearFn) => {
-    table.button("Auto-Connect All", run(() => { exports.autoConnectAll(the, getLinks, lvt); })).size(180, 40).row();
-    table.button("Clear All Links", run(() => { the.configure(clearFn()); })).size(180, 40).row();
+    table.button("Auto-Connect All",       run(() => { autoConnect(the, getLinks, lvt, null); })).size(180, 40).row();
+    table.button("Auto-Connect Turrets",   run(() => { autoConnect(the, getLinks, lvt, b => b.block.category == Category.turret); })).size(180, 40).row();
+    table.button("Auto-Connect Factories", run(() => { autoConnect(the, getLinks, lvt, b => b.block.category == Category.units); })).size(180, 40).row();
+    table.button("Clear All Links",        run(() => { the.configure(clearFn()); })).size(180, 40).row();
 };
 exports.newEffect = (lifetime, renderer) => new Effect(lifetime, cons(renderer));
 exports.cons2 = (func) => new Cons2({ get: (v1, v2) => func(v1, v2) });
